58ff56db0ecd6db2e8d6657b2602588071a2d496
Thomas Pollet <thomas.pollet@gmail.com>
2017-12-05 13:05:56 +0100

    jsonapi

--- safrs/swagger_doc.py
+++ safrs/swagger_doc.py
@@ -259,6 +259,9 @@ def swagger_relationship_doc(cls, tags = None):
                                                             { 'type' : child_class.__name__  , 'id' : child_class.sample_id() } 
                                                            ] 
                                                 })
+            el_post_schema = schema_from_dict('{} Relationship'.format(class_name), 
+                                                { 
+                                                })
             parameters.append( {
                                     'name': '{} body'.format(class_name),
                                     'in': 'body',
