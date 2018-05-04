20642d9544bfcc068b3075be76e0a8f957ff9ba9
Thomas Pollet <thomas.pollet@gmail.com>
2018-05-04 07:32:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 # python setup.py sdist
 # twine upload dist/*
 from distutils.core import setup
-from pip.req import parse_requirements
-
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_requires=[str(ir.req) for ir in parse_requirements('requirements.txt', session=False)]
 
