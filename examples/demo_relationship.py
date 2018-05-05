ef0dd602d0a266bc780e45c90d582005e2176b17
Thomas Pollet <thomas.pollet@gmail.com>
2018-05-05 18:52:49 +0200

    jsonapi_rpc demo

--- examples/demo_relationship.py
+++ examples/demo_relationship.py
@@ -1,94 +1,93 @@
 #!/usr/bin/env python
-#
-# This is a demo application to demonstrate the functionality of the safrs_rest REST API
-#
-# It can be ran standalone like this:
-# python demo_relationship.py [Listener-IP]
-#
-# This will run the example on http://Listener-Ip:5000
-#
-# - A database is created and a user is added
-# - A rest api is available
-# - swagger documentation is generated
-#
-import sys, logging, builtins
+'''
+  This demo application demonstrates the functionality of the safrs documented REST API
+  After installing safrs with pip, you can run this app standalone:
+  $ python3 demo_relationship.py [Listener-IP]
+
+  This will run the example on http://Listener-Ip:5000
+
+  - A database is created and a user is added
+  - A rest api is available
+  - swagger documentation is generated
+'''
+import sys
+import logging
+import builtins
 from flask import Flask, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_swagger_ui import get_swaggerui_blueprint
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
-from safrs.db import SAFRSBase, documented_api_method
+from flask_cors import CORS
+from safrs.db import SAFRSBase, documented_api_method, jsonapi_rpc
 from safrs.jsonapi import SAFRSJSONEncoder, Api
-
-app = Flask('safrs_demo_app')
-app.config.update( SQLALCHEMY_DATABASE_URI = 'sqlite://' )
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 # Example sqla database object
-class User(db.Model, SAFRSBase):
+class User(SAFRSBase, db.Model):
     '''
         description: User description
     '''
     __tablename__ = 'Users'
-    id = Column(String, primary_key=True)
-    name = Column(String, default = '')
-    email = Column(String, default = '')
-    books = db.relationship('Book', back_populates = "user" , lazy='dynamic')
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, default='')
+    email = db.Column(db.String, default='')
+    books = db.relationship('Book', back_populates="user", lazy='dynamic')
 
-    # Following method is exposed through the REST API 
+    # Following method is exposed through the REST API
     # This means it can be invoked with a HTTP POST
-    @documented_api_method
-    def send_mail(self, email):
-        '''
-            description : Send an email
-            args:
-                email:
-                    type : string 
-                    example : test email
-        '''
-        content = 'Mail to {} : {}\n'.format(self.name, email)
-        with open('/tmp/mail.txt', 'a+') as mailfile : 
-            mailfile.write(content)
-        return { 'result' : 'sent {}'.format(content)}
+    @classmethod
+    @jsonapi_rpc(http_methods = ['GET'])
+    def send_mail(self, **args):
+        print(args)
+        return {'result' : args}
 
-class Book(db.Model, SAFRSBase):
+class Book(SAFRSBase, db.Model):
     '''
         description: Book description
     '''
     __tablename__ = 'Books'
-    id = Column(String, primary_key=True)
-    name = Column(String, default = '')
-    user_id = Column(String, ForeignKey('Users.id'))
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, default='')
+    user_id = db.Column(db.String, db.ForeignKey('Users.id'))
     user = db.relationship('User', back_populates='books')
 
 
-# Create the database
-db.create_all()
+if __name__ == '__main__':
+    HOST = sys.argv[1] if len(sys.argv) > 1 else '0.0.0.0'
+    PORT = 5000
+    app = Flask('SAFRS Demo Application')
+    CORS(app,
+         origins="*",
+         allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+         supports_credentials=True)
+    app.config.update(SQLALCHEMY_DATABASE_URI='sqlite://', DEBUG=True)
+    db.init_app(app)
+    db.app = app
+    # Create the database
+    db.create_all()
+    API_PREFIX = '/api'
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    logging.getLogger(__name__).setLevel(logging.DEBUG)
+    builtins.log = log
 
-HOST = sys.argv[1] if len(sys.argv) > 1 else '0.0.0.0'
-PORT = 5000
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-builtins.log = log
+    with app.app_context():
+        # Create a user and a book and add the book to the user.books relationship
+        user = User(name='thomas', email='em@il')
+        book = Book(name='test_book')
+        user.books.append(book)
 
-with app.app_context():
-    # Create a user
-    user = User(name='thomas',email='em@il')
-    book = Book(name='test_book')
-    user.books.append(book)
-
-    api  = Api(app, api_spec_url = '/api/swagger', host = '{}:{}'.format(HOST,PORT), schemes = [ "http" ] )
-    # Expose the User object 
-    api.expose_object(User)
-    api.expose_object(Book)
-    # Set the JSON encoder used for object to json marshalling
-    app.json_encoder = SAFRSJSONEncoder
-    # Register the API at /api/docs
-    swaggerui_blueprint = get_swaggerui_blueprint('/api', '/api/swagger.json')
-    app.register_blueprint(swaggerui_blueprint, url_prefix='/api')
-
-    @app.route('/')
-    def goto_api():
-        return redirect('/api')
-
-    print('Starting API: http://{}:{}/api'.format(HOST,PORT))
-    app.run(host=HOST, port = PORT)
+        api = Api(app, api_spec_url=API_PREFIX + '/swagger', host='{}:{}'.format(HOST, PORT))
+        # Expose the database objects as REST API endpoints
+        api.expose_object(User)
+        api.expose_object(Book)
+        # Set the JSON encoder used for object to json marshalling
+        app.json_encoder = SAFRSJSONEncoder
+        @app.route('/')
+        def goto_api():
+            '''Create a redirect from / to /api'''
+            return redirect(API_PREFIX)
+        # Register the API at /api/docs
+        swaggerui_blueprint = get_swaggerui_blueprint(API_PREFIX, API_PREFIX + '/swagger.json')
+        app.register_blueprint(swaggerui_blueprint, url_prefix=API_PREFIX)
+        print('Starting API: http://{}:{}{}'.format(HOST, PORT, API_PREFIX))
+        app.run(host=HOST, port=PORT)
