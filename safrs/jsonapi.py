55ade30e86058964bedfc0c62432097b6585ab00
Thomas Pollet <thomas.pollet@gmail.com>
2018-06-13 14:29:36 +0200

    check included

--- safrs/jsonapi.py
+++ safrs/jsonapi.py
@@ -604,8 +604,8 @@ def get_included(data, limit):
 
         if relationship in [ r.key for r in instance._s_relationships]:
             included = getattr(instance,relationship)
-            if isinstance(included, list):
-                result  += included
+            if included:
+                result += included
 
     return result
 
