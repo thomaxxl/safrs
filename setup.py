0ae2615649fa7b4207b62a0f2af1cfec6ac94709
Thomas Pollet <thomas.pollet@gmail.com>
2018-08-21 13:53:22 +0200

    auth demo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ install_requires=[str(ir.req) for ir in parse_requirements('requirements.txt', s
 setup(
   name = 'safrs',
   packages = ['safrs'],
-  version = '1.0.20',
+  version = '1.0.21',
   license = 'MIT',
   description = 'safrs : SqlAlchemy Flask-Restful Swagger2',
   long_description=open('README.rst').read(),
