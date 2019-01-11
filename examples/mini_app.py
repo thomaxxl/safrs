28f9db56f9e815ae36c1a5d3fda80887d37fe93f
Thomas Pollet <thomas.pollet@gmail.com>
2019-01-11 11:43:22 +0100

    add main

--- examples/mini_app.py
+++ examples/mini_app.py
@@ -20,16 +20,19 @@ class User(SAFRSBase, db.Model):
 def create_api(app, HOST='localhost', PORT=5000, API_PREFIX=''):
     api = SAFRSAPI(app, host=HOST, port=PORT, prefix=API_PREFIX)
     api.expose_object(User)
-    user = User(name='test',email='em@il')
+    user = User(name='test',email='email@x.org')
     print('Starting API: http://{}:{}/{}'.format(HOST,PORT,API_PREFIX))
 
 
-def create_app(config_filename):
+def create_app(config_filename = None):
     app = Flask('demo_app')
     app.config.update( SQLALCHEMY_DATABASE_URI = 'sqlite://', DEBUG=True )
     db.init_app(app)
     with app.app_context():
         db.create_all()
         create_api(app)
-    
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
