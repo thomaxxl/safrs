a566623d14aa962be6862dfbd05e2f056d6aa566
Thomas Pollet <thomas.pollet@gmail.com>
2019-06-14 13:05:02 +0200

    duplicate() jsonapi_rpc method

--- safrs/api_methods.py
+++ safrs/api_methods.py
@@ -5,6 +5,51 @@ from sqlalchemy import or_
 from .jsonapi import SAFRSFormattedResponse, paginate, jsonapi_format_response
 from .swagger_doc import jsonapi_rpc
 from .errors import GenericError, ValidationError
+import safrs
+
+
+@classmethod
+@jsonapi_rpc(http_methods=["POST"])
+def search(cls, **kwargs):
+    """
+        pageable: True
+        description : lookup column names
+        args:
+            col_name: value
+    """
+    query = kwargs.get("query", "")
+    response = SAFRSFormattedResponse()
+    if ":" in query:
+        column_name, value = query.split(":")
+        result = cls.query.filter(
+            or_(column.like("%" + value + "%") for column in cls._s_columns if column.name == column_name)
+        )
+    else:
+        result = cls.query.filter(or_(column.like("%" + query + "%") for column in cls._s_columns))
+    instances = result
+    links, instances, count = paginate(instances)
+    data = [item for item in instances]
+    meta = {}
+    errors = None
+    response.response = jsonapi_format_response(data, meta, links, errors, count)
+    return response
+
+
+@jsonapi_rpc(http_methods=["POST"])
+def duplicate(self):
+    """
+        description: Duplicate an object - copy it and give it a new id
+    """
+    from sqlalchemy.orm.session import make_transient
+    session = safrs.DB.session
+    session.expunge(self)
+    make_transient(self)
+    self.id = self.id_type()
+    session.add(self)
+    session.commit()
+    response = SAFRSFormattedResponse(self)
+    return response
+
 
 @classmethod
 @jsonapi_rpc(http_methods=['POST'])
@@ -16,7 +61,6 @@ def lookup_re_mysql(cls, **kwargs):
             name: thom.*
     """
     result = cls
-    response = SAFRSFormattedResponse()
     for key, value in kwargs.items():
         column = getattr(cls, key, None)
         if not column:
@@ -28,12 +72,12 @@ def lookup_re_mysql(cls, **kwargs):
             data = [item for item in instances]
             meta = {}
             errors = None
-            response.response = jsonapi_format_response(data, meta, links, errors, count)
 
         except Exception as exc:
             raise GenericError("Failed to execute query {}".format(exc))
 
-    return result.all()
+    return SAFRSFormattedResponse(result.all())
+
 
 @classmethod
 @jsonapi_rpc(http_methods=['POST'])
@@ -84,33 +128,6 @@ def re_search(cls, **kwargs):
     query = kwargs.get("query", "")
     response = SAFRSFormattedResponse()
     result = cls.query.filter(or_(column.op("regexp")(query) for column in cls._s_columns))
-    instances = result
-    links, instances, count = paginate(instances)
-    data = [item for item in instances]
-    meta = {}
-    errors = None
-    response.response = jsonapi_format_response(data, meta, links, errors, count)
-    return response
-
-
-@classmethod
-@jsonapi_rpc(http_methods=["POST"])
-def search(cls, **kwargs):
-    """
-        pageable: True
-        description : lookup column names
-        args:
-            col_name: value
-    """
-    query = kwargs.get("query", "")
-    response = SAFRSFormattedResponse()
-    if ":" in query:
-        column_name, value = query.split(":")
-        result = cls.query.filter(
-            or_(column.like("%" + value + "%") for column in cls._s_columns if column.name == column_name)
-        )
-    else:
-        result = cls.query.filter(or_(column.like("%" + query + "%") for column in cls._s_columns))
     instances = result
     links, instances, count = paginate(instances)
     data = [item for item in instances]
