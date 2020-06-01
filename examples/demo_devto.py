85724ed8357ebdcf879c46c2f3ebf4634ebac241
Thomas Pollet <thomas.pollet@gmail.com>
2020-06-01 08:58:34 -0700

    demo app

---
+++ examples/demo_devto.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""
+  This demo application demonstrates the functionality of the safrs documented REST API
+  After installing safrs with pip, you can run this app standalone:
+  $ python3 demo_relationship.py [Listener-IP]
+
+  This will run the example on http://Listener-Ip:5000
+
+  - A database is created and a user is added
+  - A rest api is available
+  - swagger documentation is generated
+
+"""
+import sys
+import logging
+import builtins
+from flask import Flask, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
+from safrs import SAFRSBase, SAFRSAPI, jsonapi_rpc
+
+db = SQLAlchemy()
+
+# Example sqla database object
+class User(SAFRSBase, db.Model):
+    """
+        description: User description
+    """
+
+    __tablename__ = "Users"
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, default="")
+    email = db.Column(db.String, default="")
+    books = db.relationship("Book", back_populates="user", lazy="dynamic")
+
+class Book(SAFRSBase, db.Model):
+    """
+        description: Book description
+    """
+
+    __tablename__ = "Books"
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, default="")
+    user_id = db.Column(db.String, db.ForeignKey("Users.id"))
+    user = db.relationship("User", back_populates="books")
+
+
+def create_api(app, HOST="localhost", PORT=5000, API_PREFIX=""):
+    api = SAFRSAPI(app, host=HOST, port=PORT, prefix=API_PREFIX)
+    api.expose_object(User)
+    api.expose_object(Book)
+    print("Starting API: http://{}:{}/{}".format(HOST, PORT, API_PREFIX))
+
+
+def create_app(config_filename=None, host="localhost"):
+    app = Flask("demo_app")
+    app.config.update(SQLALCHEMY_DATABASE_URI="sqlite://")
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        # Populate the db with users and a books and add the book to the user.books relationship
+        for i in range(200):
+            user = User(name=f"user{i}", email=f"email{i}@dev.to")
+            book = Book(name="test_book")
+            user.books.append(book)
+
+        create_api(app, host)
+    return app
+
+
+host = "127.0.0.1"  # address where the api will be hosted, change this if you're not running the app on localhost!
+app = create_app(host=host)
+
+if __name__ == "__main__":
+    app.run(host=host)
+
