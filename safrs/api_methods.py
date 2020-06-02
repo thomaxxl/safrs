f838358fa51155ac52d67314e64934daa4804a89
Thomas Pollet <thomas.pollet@gmail.com>
2020-06-02 07:05:26 -0700

    comments & refactor

--- safrs/api_methods.py
+++ safrs/api_methods.py
@@ -1,7 +1,9 @@
-"""
-.. api_methods::
-"""
+# -*- coding: utf-8 -*-
+#
+# jsonapi_rpc methods that can be added to the exposed classes
+#
 from sqlalchemy import or_
+from sqlalchemy.orm.session import make_transient
 import safrs
 from .jsonapi import SAFRSFormattedResponse, paginate, jsonapi_format_response, jsonapi_sort
 from .swagger_doc import jsonapi_rpc
@@ -18,7 +20,6 @@ def search(cls, **kwargs):
             col_name: value
     """
     query = kwargs.get("query", "")
-    response = SAFRSFormattedResponse()
     if ":" in query:
         column_name, value = query.split(":")
         result = cls.query.filter(or_(column.like("%" + value + "%") for column in cls._s_columns if column.name == column_name))
@@ -29,7 +30,7 @@ def search(cls, **kwargs):
     data = [item for item in instances]
     meta = {}
     errors = None
-    response.response = jsonapi_format_response(data, meta, links, errors, count)
+    response = SAFRSFormattedResponse(data, meta, links, errors, count)
     return response
 
 
@@ -38,8 +39,6 @@ def duplicate(self):
     """
         description: Duplicate an object - copy it and give it a new id
     """
-    from sqlalchemy.orm.session import make_transient
-
     session = safrs.DB.session
     session.expunge(self)
     make_transient(self)
@@ -66,7 +65,6 @@ def lookup_re_mysql(cls, **kwargs):
             raise ValidationError('Invalid Column "{}"'.format(key))
         try:
             result = result.query.filter(column.op("regexp")(value))
-
         except Exception as exc:
             raise GenericError("Failed to execute query {}".format(exc))
 
@@ -90,7 +88,7 @@ def startswith(cls, **kwargs):
         data = [item for item in instances]
         meta = {}
         errors = None
-        response.response = jsonapi_format_response(data, meta, links, errors, count)
+        response = SAFRSFormattedResponse(data, meta, links, errors, count)
     except Exception as exc:
         raise GenericError("Failed to execute query {}".format(exc))
 
@@ -104,7 +102,7 @@ def startswith(cls, **kwargs):
             data = [item for item in instances]
             meta = {}
             errors = None
-            response.response = jsonapi_format_response(data, meta, links, errors, count)
+            response = SAFRSFormattedResponse(data, meta, links, errors, count)
 
         except Exception as exc:
             raise GenericError("Failed to execute query {}".format(exc))
@@ -121,12 +119,11 @@ def re_search(cls, **kwargs):
             query: search.*all
     """
     query = kwargs.get("query", "")
-    response = SAFRSFormattedResponse()
     result = cls.query.filter(or_(column.op("regexp")(query) for column in cls._s_columns))
     instances = result
     links, instances, count = paginate(instances)
     data = [item for item in instances]
     meta = {}
     errors = None
-    response.response = jsonapi_format_response(data, meta, links, errors, count)
+    response = SAFRSFormattedResponse(data, meta, links, errors, count)
     return response
