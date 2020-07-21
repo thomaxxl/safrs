bd1628271a15d3787b63b49de1227b2c7ab83be5
Thomas Pollet <thomas.pollet@gmail.com>
2020-07-21 12:54:11 -0700

    split files

---
+++ safrs/jsonapi_attr.py
@@ -0,0 +1,57 @@
+"""
+    jsonapi_attr: custom jsonapi attributes
+"""
+from sqlalchemy.ext.hybrid import hybrid_property
+from .swagger_doc import parse_object_doc
+
+JSONAPI_ATTR_TAG = "_s_is_jsonapi_attr"
+
+
+class jsonapi_attr(hybrid_property):
+    """
+       hybrid_property type: sqlalchemy.orm.attributes.create_proxied_attribute.<locals>.Proxy
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+            :param attr: `SAFRSBase` attribute that should be exposed by the jsonapi
+            :return: jsonapi attribute decorator
+
+            set `swagger_type` and `default` to customize the swagger
+        """
+        setattr(self, JSONAPI_ATTR_TAG, True)
+
+        if args:
+            # called when the app starts
+            attr = args[0]
+            obj_doc = parse_object_doc(attr)
+            if isinstance(obj_doc, dict):
+                for k, v in obj_doc.items():
+                    setattr(self, k, v)
+        else:
+            # the "default" kwarg may have been added by the obj_doc but we no longer
+            # need it (and it causes an exception)
+            kwargs.pop("default", None)
+        super().__init__(*args, **kwargs)
+
+    def getter(self, fget):
+        """
+            Provide a decorator that defines a getter method.
+        """
+
+        return self._copy(fget=fget)
+
+    def setter(self, fset):
+        """
+            Provide a decorator that defines a setter method.
+        """
+
+        return self._copy(fset=fset)
+
+
+def is_jsonapi_attr(attr):
+    """
+        :param attr: `SAFRSBase` `jsonapi_attr` decorated attribute
+        :return: boolean
+    """
+    return getattr(attr, JSONAPI_ATTR_TAG, False) is True
---
+++ safrs/jsonapi_filters.py
@@ -0,0 +1,66 @@
+"""
+JSON:API filtering strategy
+"""
+from .config import get_request_param
+import sqlalchemy
+import safrs
+from .jsonapi_attr import is_jsonapi_attr
+
+
+@classmethod
+def jsonapi_filter(cls):
+    """
+        https://jsonapi.org/recommendations/#filtering
+        Apply the request.args filters to the object
+
+        :return: sqla query object
+    """
+
+    # First check if a filter= URL query parameter has been used
+    # the SAFRSObject should've implemented a filter method or
+    # overwritten the _s_filter method to implement custom filtering
+    filter_args = get_request_param("filter")
+    if filter_args:
+        safrs_object_filter = getattr(cls, "filter", None)
+        if isinstance(cls, (list, sqlalchemy.orm.collections.InstrumentedList)):
+            # not implemented
+            result = cls
+        elif callable(safrs_object_filter):
+            result = safrs_object_filter(filter_args)
+        else:
+            result = cls._s_filter(filter_args)
+        return result
+
+    expressions = []
+    filters = get_request_param("filters", {})
+    if isinstance(cls, (list, sqlalchemy.orm.collections.InstrumentedList)):
+        return cls
+
+    for attr_name, val in filters.items():
+        if attr_name == "id":
+            return cls._s_get_instance_by_id(val)
+        if attr_name not in cls._s_jsonapi_attrs:
+            # validation failed: this attribute can't be queried
+            safrs.log.warning("Invalid filter {}".format(attr_name))
+            return []
+        else:
+            attr = cls._s_jsonapi_attrs[attr_name]
+        print(attr_name, type(val))
+        if is_jsonapi_attr(attr):
+            # to do
+            safrs.log.debug("Filtering not implemented for {}".format(attr))
+        else:
+            expressions.append((attr, val))
+
+    if expressions:
+        _expressions = []
+        for column, val in expressions:
+            if hasattr(column, "in_"):
+                _expressions.append(column.in_(val.split(",")))
+            else:
+                safrs.log.warning("'{}.{}' is not a column ({})".format(cls, column, type(column)))
+        result = cls._s_query.filter(*_expressions)
+    else:
+        result = cls._s_query
+
+    return result
