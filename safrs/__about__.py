51f9f35d433501c7bdf9a12582d551c14587e5fe
Thomas Pollet <thomas.pollet@gmail.com>
2020-07-31 11:17:32 -0700

    v2.9.2

--- safrs/__about__.py
+++ safrs/__about__.py
@@ -1,2 +1,2 @@
-__version__ = "2.9.1"
+__version__ = "2.9.2"
 __description__ = "Self-documenting(OAS) JSON:API framework for flask"
--- setup.py
+++ setup.py
@@ -5,37 +5,40 @@ twine upload dist/*
 
 from distutils.core import setup
 
-with open("requirements.txt", "rt") as fp:
-    install_requires = fp.read().strip().split("\n")
+def safrs_setup(): # pragma: no cover
+    with open("requirements.txt", "rt") as fp:
+        install_requires = fp.read().strip().split("\n")
 
-version = "2.9.1"
+    version = "2.9.2"
 
-setup( # pragma: no cover
-    name="safrs",
-    packages=["safrs"],
-    version=version,
-    license="MIT",
-    description="safrs : SqlAlchemy Flask-Restful Swagger2",
-    long_description=open("README.rst").read(),
-    author="Thomas Pollet",
-    author_email="thomas.pollet@gmail.com",
-    url="https://github.com/thomaxxl/safrs",
-    download_url="https://github.com/thomaxxl/safrs/archive/{}.tar.gz".format(version),
-    keywords=["SqlAlchemy", "Flask", "REST", "Swagger", "JsonAPI", "OpenAPI"],
-    python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, <4",
-    install_requires=install_requires,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Intended Audience :: Developers",
-        "Framework :: Flask",
-        "Topic :: Software Development :: Libraries",
-        "Environment :: Web Environment",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-    ],
-)
+    setup(
+        name="safrs",
+        packages=["safrs"],
+        version=version,
+        license="MIT",
+        description="safrs : SqlAlchemy Flask-Restful Swagger2",
+        long_description=open("README.rst").read(),
+        author="Thomas Pollet",
+        author_email="thomas.pollet@gmail.com",
+        url="https://github.com/thomaxxl/safrs",
+        download_url="https://github.com/thomaxxl/safrs/archive/{}.tar.gz".format(version),
+        keywords=["SqlAlchemy", "Flask", "REST", "Swagger", "JsonAPI", "OpenAPI"],
+        python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, <4",
+        install_requires=install_requires,
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Intended Audience :: Developers",
+            "Framework :: Flask",
+            "Topic :: Software Development :: Libraries",
+            "Environment :: Web Environment",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+        ],
+    )
+
+safrs_setup()
