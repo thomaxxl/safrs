c9b6b9969e7217a4b7e40a974d662a17184137f9
Thomas Pollet <thomas.pollet@gmail.com>
2020-09-30 16:58:38 +0200

    v2.10.2

--- safrs/__about__.py
+++ safrs/__about__.py
@@ -1,2 +1,2 @@
-__version__ = "2.10.1"
+__version__ = "2.10.2"
 __description__ = "Self-documenting(OAS) JSON:API framework for flask"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def safrs_setup():
     with open("requirements.txt", "rt") as fp:
         install_requires = fp.read().strip().split("\n")
 
-    version = "2.10.1"
+    version = "2.10.2"
 
     setup(
         name="safrs",
