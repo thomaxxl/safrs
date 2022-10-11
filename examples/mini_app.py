dcc1a20c2466902eee9b1da2b5bcdad2d6bb7190
Thomas Pollet <thomas.pollet@gmail.com>
2022-10-11 14:10:36 +0200

    use 127.0.0.1 instead of localhost (avoid cors issue)

--- examples/mini_app.py
+++ examples/mini_app.py
@@ -26,7 +26,7 @@ def create_api(app, host="localhost", port=5000, prefix=""):
     print(f"Starting API: http://{host}:{port}/{prefix}")
 
 
-def create_app(host="localhost"):
+def create_app(host="127.0.0.1"):
     app = Flask("demo_app")
     app.config.update(SQLALCHEMY_DATABASE_URI="sqlite://")
     db.init_app(app)
