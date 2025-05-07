a81fc3d183f740c69f14733d7c9e739490bc177f
Thomas Pollet <thomas.pollet@gmail.com>
2025-05-07 15:28:07 +0200

    v3.1.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def safrs_setup():
     with open("requirements.txt", "rt") as fp:
         install_requires = fp.read().strip().split("\n")
 
-    version = "3.1.6"
+    version = "3.1.7"
 
     setup(
         name="safrs",
