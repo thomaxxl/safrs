fa7c477639335451f8e5200b77ee9237217c7972
Thomas Pollet <thomas.pollet@gmail.com>
2026-03-08 12:46:39 +0100

    Add grouped JSON filter engine with legacy compatibility

--- safrs/base.py
+++ safrs/base.py
@@ -182,13 +182,12 @@ import inspect
 import datetime
 import sqlalchemy
 import json
-import operator
 from http import HTTPStatus
 from urllib.parse import urljoin
 from flask import request, url_for, has_request_context, has_app_context, current_app, g
 from flask_sqlalchemy.model import Model
 from sqlalchemy.orm.session import make_transient
-from sqlalchemy import inspect as sqla_inspect, or_
+from sqlalchemy import inspect as sqla_inspect
 from sqlalchemy.orm.interfaces import ONETOMANY, MANYTOONE, MANYTOMANY
 from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
 from sqlalchemy.sql.schema import Column
@@ -207,6 +206,7 @@ from .swagger_doc import get_doc
 from .util import ClassPropertyDescriptor, classproperty
 from .model_config import SAFRSModelConfig
 from .jsonapi_context import maybe_jsonapi_context
+from .filtering import apply_filter_json
 from . import tx
 
 
@@ -1540,42 +1540,10 @@ class SAFRSBase(Model):
         - val: The value that you want to compare.
         :return: sqla query object
         """
-        try:
-            filters = json.loads(filter_args[0])
-        except json.decoder.JSONDecodeError:
+        if not filter_args:
             raise ValidationError("Invalid filter format (see https://github.com/thomaxxl/safrs/wiki)")
-
-        if not isinstance(filters, list):
-            filters = [filters]
-
-        expressions = []
-        query = cls._s_query
-
-        for filt in filters:
-            if not isinstance(filt, dict):
-                safrs.log.warning(f"Invalid filter '{filt}'")
-                continue
-            attr_name = filt.get("name")
-            attr_val = filt.get("val")
-            if attr_name != "id" and attr_name not in cls._s_jsonapi_attrs:
-                raise ValidationError(f'Invalid filter "{filt}", unknown attribute "{attr_name}"')
-
-            op_name = filt.get("op", "").strip("_")
-            attr = cls._s_jsonapi_attrs[attr_name] if attr_name != "id" else cls.id
-            if op_name in ["in", "notin"]:
-                op = getattr(attr, op_name + "_")
-                query = query.filter(op(attr_val))
-            elif op_name in ["like", "ilike", "match", "notilike"] and hasattr(attr, "like"):
-                # => attr is Column or InstrumentedAttribute
-                like = getattr(attr, op_name)
-                expressions.append(like(attr_val))
-            elif not hasattr(operator, op_name):
-                raise ValidationError(f'Invalid filter "{filt}", unknown operator "{op_name}"')
-            else:
-                op = getattr(operator, op_name)
-                expressions.append(op(attr, attr_val))
-
-        return query.filter(or_(*expressions))
+        raw_filter = str(filter_args[0])
+        return apply_filter_json(cls, raw_filter, cls._s_query)
 
 
 class Included:
